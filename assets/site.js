2edead8c5895a603869a8eba004456500716bf1b
Tommy Carlier <T.Carlier@televic.com>
2014-11-25 10:38:44 +0100

    Made script simpler

--- assets/site.js
+++ assets/site.js
@@ -4,13 +4,12 @@
   
   "use strict";
   
-  var re_http = /^http:/, re_tcx = /^http:\/\/www\.tcx\.be\//;
-  
   // set target to _blank for all external hyperlinks
-  for (var xs = d.getElementsByTagName('A'), i = xs.length; i--; ){
-    var x = xs[i], href = x.href;
-    if (re_http.test(href) && !re_tcx.test(href)){
-      x.setAttribute('target', '_blank');
+  var hn = location.hostname;
+  for (var xs = d.links, i = xs.length; i--; ){
+    var x = xs[i];
+    if (x.hostname != hn){
+      x.target = '_blank';
     }
   }
   
