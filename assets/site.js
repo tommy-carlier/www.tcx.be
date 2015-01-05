a974015fa13e4ca73f113fed311a01264003bad8
Tommy Carlier <T.Carlier@televic.com>
2015-01-05 10:19:20 +0100

    Make copyright date in footer dynamic

--- assets/site.js
+++ assets/site.js
@@ -2,12 +2,21 @@
   
   "use strict";
   
+  var x, h = location.host;
   // set target to _blank for all external hyperlinks
-  var h = location.host;
   for (var xs = d.links, i = xs.length; i--; ){
     var x = xs[i];
     if (x.host != h){
       x.target = '_blank';
+    }
+  }
+  
+  // set current year in copyright footer
+  var q = d.querySelector;
+  if (q){
+    x = q('body>footer>time');
+    if (x){
+      x.innerHTML = (new Date()).getFullYear();
     }
   }
   
