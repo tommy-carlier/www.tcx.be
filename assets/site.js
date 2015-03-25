89bf8867391a8485f0943caf83f3819a1edc2d64
Tommy Carlier <T.Carlier@televic.com>
2015-03-25 10:11:23 +0100

    Added some non-breaking spaces and hyphens

--- assets/site.js
+++ assets/site.js
@@ -1,22 +1,26 @@
-(function(d){
+(function(){
   
   "use strict";
   
-  var x, h = location.host;
-  // set target to _blank for all external hyperlinks
-  for (var xs = d.links, i = xs.length; i--; ){
-    var x = xs[i];
-    if (x.host != h){
-      x.target = '_blank';
+  var d = document;
+  
+  function initPage(){
+    var x, h = location.host;
+    // set target to _blank for all external hyperlinks
+    for (var xs = d.links, i = xs.length; i--; ){
+      var x = xs[i];
+      if (x.host != h){
+        x.target = '_blank';
+      }
+    }
+
+    // set current year in copyright footer
+    if (d.querySelector){
+      x = d.querySelector('body>footer>time');
+      if (x){
+        x.innerHTML = (new Date()).getFullYear();
+      }
     }
   }
   
-  // set current year in copyright footer
-  if (d.querySelector){
-    x = d.querySelector('body>footer>time');
-    if (x){
-      x.innerHTML = (new Date()).getFullYear();
-    }
-  }
-  
-}(document))
+}())
