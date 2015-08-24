2f7f989b0d2b2e4b2a2abd6573d11dda155b5da7
Tommy Carlier <tommy-carlier@users.noreply.github.com>
2015-08-24 10:43:43 +0200

    Change "active" to "current" (to avoid confusion with CSS :active pseudoclass)

--- assets/site.js
+++ assets/site.js
@@ -24,16 +24,16 @@
     }
   });
   
-  // initializer to set active tab in page header
+  // initializer to indicate current tab in page header
   inits.push(function(){
     if (d.querySelectorAll){
       var xs = d.querySelectorAll('body>header>nav>a'), href = location.href;
       for(var i = 1, n = xs.length; i < n; i++){
         var x = xs[i];
         if (href.startsWith(x.href)){
-          x.classList.add('active');
+          x.classList.add('current');
         } else {
-          x.classList.remove('active');
+          x.classList.remove('current');
         }
       }
     }
