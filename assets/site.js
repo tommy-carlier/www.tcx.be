35c1be5f6b6b7a7d681cf0aedf132fef6fcb7a82
tommy-carlier <tommy@tcx.be>
2021-03-16 08:25:21 +0100

    Change contact form

--- assets/site.js
+++ assets/site.js
@@ -9,56 +9,26 @@
     }
   }
   
-  var d = document, inits = [];
-  window.initializers = inits;
+  var d = document;
   
-  // initializer to manipulate all hyperlinks
-  inits.push(function(){
-    var h = location.host, t;
-    for (var xs = d.links, i = xs.length; i--; ){
-      var x = xs[i], m = x.getAttribute('data-m');
-      if (m) {
-        // de-obfuscate e-mail hyperlink
-        m = m.replace(/[\/]/g, '@').replace(/,/g, '.');
-        x.href = 'mailto:' + m;
-        if ((t = x.firstChild) && t.nodeType == 3){
-          t.data = m;
-        }
+  // indicate current tab in page header
+  if (d.querySelectorAll && d.classList){
+    var xs = d.querySelectorAll('body>header>nav>a'), href = location.href;
+    for(var i = 1, n = xs.length; i < n; i++){
+      var x = xs[i];
+      if (href.startsWith(x.href)){
+        x.classList.add('current');
+      } else {
+        x.classList.remove('current');
       }
-    }
-  });
-  
-  // initializer to indicate current tab in page header
-  inits.push(function(){
-    if (d.querySelectorAll){
-      var xs = d.querySelectorAll('body>header>nav>a'), href = location.href;
-      for(var i = 1, n = xs.length; i < n; i++){
-        var x = xs[i];
-        if (href.startsWith(x.href)){
-          x.classList.add('current');
-        } else {
-          x.classList.remove('current');
-        }
-      }
-    }
-  });
-
-  // initializer to set current year in copyright footer
-  inits.push(function(){
-    if (d.querySelector){
-      var x = d.querySelector('body>footer>time');
-      if (x){
-        x.innerHTML = (new Date()).getFullYear();
-      }
-    }
-  });
-  
-  function initPage(){
-    // call all initializers
-    for(var i = 0, n = inits.length; i < n; i++){
-      inits[i]();
     }
   }
-  
-  initPage();
+
+  // set current year in copyright footer
+  if (d.querySelector){
+    var x = d.querySelector('body>footer>time');
+    if (x){
+      x.textContent = (new Date()).getFullYear();
+    }
+  }
 }())
